c27a9e735430d098511d8bd4c74ed0254dba3e82
Louis Heath <lheath1998@gmail.com>
2022-05-21 21:54:13 +0100

    add and validate more inputs

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,23 +1,17 @@
 document.getElementById('form').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    var age = document.getElementById("input-age").value;
-    var weight = document.getElementById("input-weight").value;
-
-    if (!isInt(age)) {
-        displayError('Age must be a whole number')
-        return
-    }
-    if (isNaN(parseFloat(weight))) {
-        displayError('Weight must be a number')
+    let [inputs, err] = readInputs()
+    if (err != '') {
+        displayError(err)
         return
     }
 
     fetch('https://onerepmaxcalculator.herokuapp.com/calculate', {
             method: 'POST',
             body: JSON.stringify({
-                age: parseInt(age),
-                weight: parseFloat(weight),
+                age: parseInt(inputs['age']),
+                weight: parseFloat(inputs['weight']),
             }),
             headers: {
                 'Content-type': 'application/json; charset=UTF-8',
@@ -27,15 +21,10 @@ document.getElementById('form').addEventListener('submit', function(event) {
         .then((response) => response.json())
         .then((json) => {
             console.log(json)
-            document.getElementById('result').textContent = 'Calulated Max: ' + json['max'] + 'kg';
+            document.getElementById('result').textContent = 'Calculated Max: ' + json['max'] + 'kg';
         });
 });
 
 function displayError(err) {
     document.getElementById('result').textContent = 'Error: ' + err;
-}
-
-function isInt(value) {
-    var er = /^-?[0-9]+$/;
-    return er.test(value);
 }
---
+++ scripts/input.js
@@ -0,0 +1,90 @@
+
+function readInputs() {
+    age = read('age')
+    err = validateAge(age)
+    if (err != '') {
+        return null, err
+    }
+    gender = read('gender')
+    err = validateGender(gender)
+    if (err != '') {
+        return null, err
+    }
+    height = read('height')
+    err = validateHeight(height)
+    if (err != '') {
+        return null, err
+    }
+    weight = read('weight')
+    err = validateWeight(weight)
+    if (err != '') {
+        return null, err
+    }
+    lift = read('lift')
+    err = validateLift(lift)
+    if (err != '') {
+        return null, err
+    }
+    return [{
+        'age': age,
+        'gender': gender,
+        'height': height,
+        'weight': weight,
+        'lift': lift,
+    }, '']
+}
+
+function read(param) {
+    return document.getElementById(`input-${param}`).value;
+}
+
+function validateAge(a) {
+    if (!isInt(a)) {
+        return 'Age must be a whole number'
+    }
+    return ''
+}
+
+function validateGender(g) {
+    if (g != 'Male' && g != 'Female' && g != 'Non-binary/Other') {
+        return `Gender must be Male, Female or Non-binary/Other`
+    }
+    return ''
+}
+
+function validateWeight(w) {
+    if (w == '') {
+        return ''
+    }
+    if (isNaN(parseFloat(weight))) {
+        return 'Weight must be a number'
+    }
+    return ''
+}
+
+function validateHeight(h) {
+    if (h == '') {
+        return ''
+    }
+    if (isNaN(parseFloat(h))) {
+        return 'Height must be a number'
+    }
+    return ''
+}
+
+function validateLift(l) {
+    if (l == '') {
+        return ''
+    }
+    if (l != 'Bench' && l != 'Squat' && l != 'Deadlift' && l != 'Other') {
+        return `Lift must be Bench, Squat, Deadlift or Other`
+    }
+    return ''
+}
+
+function isInt(value) {
+    var er = /^-?[0-9]+$/;
+    return er.test(value);
+}
+
+
